9aaa5e90841a72eac0a0de65c8cc724598341694
Fodgek <nikita.serebro@mail.ru>
2024-03-12 21:25:35 +1000

    Интерфейс для карты пока пустой

---
+++ RogueLike_Remake/GameScene/Map/IMap.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RogueLike_Remake.GameScene.Map
+{
+    internal interface IMap
+    {
+    }
+}
